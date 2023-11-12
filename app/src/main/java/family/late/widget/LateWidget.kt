363cef23ca9bbff4f37f7ed35cf8f7495453fdfa
GeoffreyXue <xuegeoff@gmail.com>
2023-11-12 11:47:31 -0600

    Update widget to be more dynamic with refresh, exclude declined recipients, make message better

--- app/src/main/java/family/late/widget/LateWidget.kt
+++ app/src/main/java/family/late/widget/LateWidget.kt
@@ -1,9 +1,10 @@
 package family.late.widget
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
+import android.os.Handler
 import android.util.Log
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -18,6 +19,7 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.glance.Button
+import androidx.glance.ButtonDefaults
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
@@ -26,13 +28,13 @@ import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.SizeMode
 import androidx.glance.appwidget.action.ActionCallback
-import androidx.glance.appwidget.action.actionRunCallback
 import androidx.glance.appwidget.cornerRadius
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Box
 import androidx.glance.layout.Column
+import androidx.glance.layout.fillMaxHeight
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.padding
@@ -52,6 +54,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.time.Duration
 import java.time.Instant
+import java.util.Timer
 import kotlin.time.Duration.Companion.seconds
 
 
@@ -102,6 +105,7 @@ fun ContentWrapper(context: Context) {
     Content(email, calendarId, accessToken) { refresh() }
 }
 
+@SuppressLint("ResourceType")
 @Composable
 fun Content(sender: String?, calendarId: String?, accessToken: String?, refresh: () -> Unit) {
     val coroutineScope = rememberCoroutineScope()
@@ -127,8 +131,12 @@ fun Content(sender: String?, calendarId: String?, accessToken: String?, refresh:
             .minByOrNull { it.start.dateTime.value }
     }
 
+    // Repeatedly refetch and update time every minute
     LaunchedEffect(Unit) {
-        fetchNextEvent()
+        coroutineScope.launch {
+            fetchNextEvent()
+            delay(60000)
+        }
     }
 
     LaunchedEffect(sender, calendarId, accessToken, nextEvent) {
@@ -155,63 +163,75 @@ fun Content(sender: String?, calendarId: String?, accessToken: String?, refresh:
 
     Box(
         modifier = GlanceModifier.background(ColorProvider(backgroundColor))
-            .clickable {
-                text = "pinging..."
-                coroutineScope.launch {
-                    if (sender == null || accessToken == null || nextEvent == null || nextEvent?.attendees == null) return@launch
-                    val subject = "Late to meeting - ${nextEvent!!.summary}"
-                    val message = "Sorry, I will be a few minutes late to this meeting."
-                    // List of recipients includes all but sender
-                    val recipients = nextEvent!!.attendees.map { it.email }.filter { !it.equals(sender)}
-
-                    if (sendEmail(accessToken, sender, subject, message, recipients)) {
-                        text = "Successfully sent email!"
-                        backgroundColor = Green80
-                    }
-                    else {
-                        text = "Failed to send email."
-                        backgroundColor = Red80
-                    }
-                    delay(2.seconds)
-                    text = ""
-                    fetchNextEvent()
-                }
-            }
-            .fillMaxSize(),
+            .fillMaxSize()
+            .cornerRadius(20.dp),
         contentAlignment = Alignment.Center,
     ) {
 
-        Column {
-//            Button(
-//                text = "Refresh",
-//                onClick = { refresh() },
-//                style = TextStyle(color = ColorProvider(Color.Black)),
-//                modifier = GlanceModifier.background(ColorProvider(Color.White))
-//            )
+        Column( modifier = GlanceModifier.fillMaxHeight() ) {
+            Button(
+                text = "Refresh",
+                onClick = { refresh() },
+                colors = ButtonDefaults.buttonColors(backgroundColor = ColorProvider(Color.Transparent),
+                    contentColor = ColorProvider(Color.White)
+                ),
+                modifier = GlanceModifier.fillMaxWidth(),
+                style = TextStyle(textAlign = TextAlign.Right)
+            )
 
-            if (text.isNotEmpty()) {
-                Text(
-                    text = text,
-                    style = TextStyle(color = ColorProvider(MaterialTheme.colorScheme.onPrimary),
-                        fontSize = TextUnit(32F, TextUnitType.Sp),
-                        fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Center),
-                    modifier = GlanceModifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                )
+            Box (
+                modifier = GlanceModifier.clickable {
+                    text = "Sending Email..."
+                    coroutineScope.launch {
+                        if (sender == null || accessToken == null || nextEvent == null || nextEvent?.attendees == null) return@launch
+                        val subject = "Running Late to meeting - ${nextEvent!!.summary}"
+                        val message = "I hope this email finds you well. I wanted to inform you that I will be running a bit late for our upcoming meeting. I apologize for any inconvenience this may cause."
+                        // List of recipients includes all but sender
+                        val recipients = nextEvent!!.attendees
+                            .filter { it.responseStatus != "declined"}
+                            .map { it.email }
+                            .filter { !it.equals(sender)}
 
-                if (text != RECONFIGURE) {
-                    Text(
-                        text = "Send email!",
-                        style = TextStyle(color = ColorProvider(MaterialTheme.colorScheme.onPrimary),
-                            fontSize = TextUnit(16F, TextUnitType.Sp),
-                            fontWeight = FontWeight.Bold,
-                            textAlign = TextAlign.Center),
-                        modifier = GlanceModifier
-                            .fillMaxWidth()
-                            .padding(8.dp)
-                    )
+                        if (sendEmail(accessToken, sender, subject, message, recipients)) {
+                            text = "Successfully sent email!"
+                            backgroundColor = Green80
+                        }
+                        else {
+                            text = "Failed to send email."
+                            backgroundColor = Red80
+                        }
+                        delay(2.seconds)
+                        text = ""
+                        fetchNextEvent()
+                    }
+                }
+            ) {
+                Column() {
+                    if (text.isNotEmpty()) {
+                        Text(
+                            text = text,
+                            style = TextStyle(color = ColorProvider(MaterialTheme.colorScheme.onPrimary),
+                                fontSize = TextUnit(32F, TextUnitType.Sp),
+                                fontWeight = FontWeight.Bold,
+                                textAlign = TextAlign.Center),
+                            modifier = GlanceModifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        )
+
+                        if (text != RECONFIGURE) {
+                            Text(
+                                text = "Send email!",
+                                style = TextStyle(color = ColorProvider(MaterialTheme.colorScheme.onPrimary),
+                                    fontSize = TextUnit(16F, TextUnitType.Sp),
+                                    fontWeight = FontWeight.Bold,
+                                    textAlign = TextAlign.Center),
+                                modifier = GlanceModifier
+                                    .fillMaxWidth()
+                                    .padding(8.dp)
+                            )
+                        }
+                    }
                 }
             }
         }
